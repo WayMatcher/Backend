36a7695f8c29ad7debe7252eafd40612427ecb2c
J00lo <chris.rudi003@gmail.com>
2025-03-04 14:48:59 +0100

    added and configured ConfigurationService

---
+++ WayMatcher/Services/ConfigurationService.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace WayMatcherBL.Services
+{
+    public class ConfigurationService
+    {
+        private readonly IConfiguration _configuration;
+
+        public ConfigurationService()
+        {
+            _configuration = new ConfigurationBuilder()
+                .SetBasePath("C:\\Users\\christian.rudigier\\source\\repos\\WayMatcher\\Backend\\WayMatcher")
+                .AddJsonFile("appsettings.json")
+                .Build();
+        }
+
+        public string GetConnectionString(string name)
+        {
+            return _configuration.GetConnectionString(name);
+        }
+    }
+}   
